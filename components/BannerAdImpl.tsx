d3f57414fb32fd464e27dc416c22067759417a4b
Desmond <desmondhei25@gmail.com>
2025-10-27 14:28:38 +0800

    commit

--- components/BannerAdImpl.tsx
+++ components/BannerAdImpl.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+import React from "react";
+import { Platform } from "react-native";
+import {
+  BannerAd,
+  BannerAdSize,
+  TestIds,
+} from "react-native-google-mobile-ads";
 
 export default function BannerAdImpl() {
+  const adUnitId = Platform.select({
+    ios: "ca-app-pub-7106153117550777/5330249389", // Replace with your iOS banner ad unit ID
+    default: "",
+  });
   return (
     <BannerAd
-      unitId={TestIds.BANNER}
+      // unitId={TestIds.BANNER}
+      unitId={adUnitId}
       size={BannerAdSize.BANNER}
       requestOptions={{
         requestNonPersonalizedAdsOnly: true,
